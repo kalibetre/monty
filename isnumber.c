55eadbfbd3abd48be1ae0574a14e239b23b85cea
kalbetre-alx <kalbetre@icloud.com>
2022-09-07 08:18:38 +0000

    add number check function implementation

---
+++ isnumber.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * isnumber - checks if a string is a valid number
+ * @str: the string
+ *
+ * Return: 1 if it is a valid number or 0
+ */
+int isnumber(char *str)
+{
+	size_t i = 0;
+
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+
+	for (; i < strlen(str); i++)
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+			continue;
+		return (0);
+	}
+	return (1);
+}
