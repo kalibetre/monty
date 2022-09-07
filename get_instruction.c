6a93a7b103ad03a3ca6d0d4f97231e0829191509
kalbetre-alx <kalbetre@icloud.com>
2022-09-07 08:18:05 +0000

    add instruction handlers

---
+++ get_instruction.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * get_instruction - Get the instruction object
+ * @opcode: the instruction opcode
+ *
+ * Return: the instruction or NULL
+ */
+void (*get_instruction(char *opcode))(stack_t **stack, unsigned int l_num)
+{
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pop", pop},
+		{"pall", pall},
+	};
+	int i = 0;
+
+	while (i < 3 && instructions[i].opcode != NULL)
+	{
+		if (strcmp(instructions[i].opcode, opcode) == 0)
+			return (instructions[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ instructions.c
@@ -0,0 +1,70 @@
+#include "monty.h"
+/**
+ * pop - pop opcode instruction
+ * @stack: the stack
+ * @line_number: line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+
+	(void) line_number;
+	if (*stack != NULL)
+	{
+		node = *stack;
+		*stack = node->next;
+		if (*stack != NULL)
+			(*stack)->prev = NULL;
+		free(node);
+	}
+}
+
+/**
+ * push - push opcode instruction
+ * @stack: the stack
+ * @line_number: line number
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *args = NULL;
+	stack_t *node;
+
+	args = strtok(NULL, " ");
+	if (!isnumber(args))
+		invalid_instruction(line_number, "push");
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		malloc_error();
+
+	node->n = atoi(args);
+	node->prev = NULL;
+	node->next = NULL;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = node;
+		node->next = *stack;
+	}
+	*stack = node;
+}
+
+/**
+ * pall - pall opcode instruction
+ * @stack: the stack
+ * @line_number: line number
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	(void)line_number;
+	if (stack != NULL)
+	{
+		temp = *stack;
+		while (temp != NULL)
+		{
+			printf("%d\n", temp->n);
+			temp = temp->next;
+		}
+	}
+}
