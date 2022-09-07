9a5862de10ff227ef755ff89f83260e581d6a79d
kalbetre-alx <kalbetre@icloud.com>
2022-09-07 08:18:53 +0000

    add getline function

---
+++ _getline.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * _getline - reads a line from the standard input
+ * @line: pointer to the line to be read
+ * @line_n: number of bytes read
+ * @fd: file descriptor
+ *
+ * Return: -1 if it failed otherwise 0
+ */
+int _getline(char **line, size_t *line_n, int fd)
+{
+	char buffer[1024], c;
+	int byte_count = 0;
+	ssize_t buffer_rd = 0;
+	*line_n = 0;
+	do {
+		buffer_rd = read(fd, &c, 1);
+
+		if (buffer_rd <= 0)
+			return (-1);
+
+		if (c == '\n')
+		{
+			buffer[byte_count++] = '\0';
+			*line = malloc(sizeof(char) * byte_count);
+			(*line)[0] = '\0';
+			*line = strcat(*line, buffer);
+			*line_n = byte_count - 1;
+			break;
+		}
+
+		buffer[byte_count++] = c;
+	} while (1);
+	return (0);
+}
